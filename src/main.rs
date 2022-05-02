cc39eef6000ea126e64986ae7ff190020e970863
Dyam Marcano <dyam.marcano@gmail.com>
2022-05-02 00:12:11 -0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use qrcode_png::{Color, QrCode, QrCodeEcc};
 
-// buffer to base64 encode the qr code
+// buffer to base64 encode the qr code.
 fn buffer_to_base64(buffer: &[u8]) -> String {
     format!("data:image/png;base64,{}", base64::encode(buffer))
 }
 
+// generate qr code with parameters.
 fn generate_qr_code(text: &str, ecc: QrCodeEcc, zoom: u32, margin: u32) -> String {
     let mut qr_code = QrCode::new(text.as_bytes(), ecc).unwrap();
     qr_code.zoom(zoom).margin(margin);
@@ -13,9 +14,8 @@ fn generate_qr_code(text: &str, ecc: QrCodeEcc, zoom: u32, margin: u32) -> Strin
     buffer_to_base64(&buf)
 }
 
-
 fn main() {
-    let text = "Hello, world!";
+    let text = "Hello, Rust!";
     let result = generate_qr_code(text, QrCodeEcc::Medium, 10, 10);
     println!("{}", result);
 }
